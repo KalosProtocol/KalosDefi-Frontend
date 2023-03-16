36106902dd1d114527b8e99f8c9c83b494f8d184
Kalos Protocol <kaizendex@protonmail.com>
2023-03-16 18:47:34 +1100

    footer

--- src/views/Home/components/Footer.tsx
+++ src/views/Home/components/Footer.tsx
@@ -1,94 +1,51 @@
 import styled from 'styled-components'
-import { Flex, Heading, Text, Link, /* useMatchBreakpointsContext */ } from '@pancakeswap/uikit'
+import { Flex, Heading, Text, Link } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
-import ConnectWalletButton from 'components/ConnectWalletButton'
-// import Container from 'components/Layout/Container'
 import { useWeb3React } from '@web3-react/core'
-// import SunburstSvg from './SunburstSvg'
-// import CompositeImage from './CompositeImage'
-
-/* const BgWrapper = styled.div`
-  overflow: hidden;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0px;
-  left: 0px;
-`
-
- const StyledSunburst = styled(SunburstSvg)`
-  height: 350%;
-  width: 350%; 
-
-  ${({ theme }) => theme.mediaQueries.xl} {
-    height: 400%;
-    width: 400%;
-  } 
-` */
+import { multicallv2 } from 'utils/multicall copy'
+import xaloAbi from 'config/abi/xalo.json'
+import { BalanceWithLoading } from 'components/Balance'
+import { useState, useEffect } from 'react'
 
 const Wrapper = styled(Flex)`
   z-index: 1;
   position: relative;
   flex-direction: column;
-  align-items: start;
+  align-items: center;
   justify-content: center;
   overflow: hidden;
 `
 
-/* const FloatingPancakesWrapper = styled(Container)`
-  overflow: hidden;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  visibility: hidden;
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    visibility: visible;
-  }
-`
-
-const TopLeftImgWrapper = styled(Flex)`
-  position: absolute;
-  left: 0;
-  top: 0;
-`
-
-const BottomRightImgWrapper = styled(Flex)`
-  position: absolute;
-  right: 0;
-  bottom: 0;
-`
-
-const topLeftImage = {
-  path: '/images/home/flying-pancakes/',
-  attributes: [
-    { src: '1-bottom', alt: 'Pancake flying on the bottom' },
-    { src: '1-left', alt: 'Pancake flying on the left' },
-    { src: '1-top', alt: 'Pancake flying on the top' },
-  ],
-}
-
-const bottomRightImage = {
-  path: '/images/home/flying-pancakes/',
-  attributes: [
-    { src: '2-bottom', alt: 'Pancake flying on the bottom' },
-    { src: '2-top', alt: 'Pancake flying on the top' },
-    { src: '2-right', alt: 'Pancake flying on the right' },
-  ],
-} */
-
 const Footer = () => {
   const { t } = useTranslation()
   const { account } = useWeb3React()
-  // const { isTablet, isDesktop } = useMatchBreakpointsContext()
+  const tokenAddress = '0x4d7a5b0665EdD992852cb0dA8257A1B7C77a6983'
+  const tokenBalanceOf = '0x70a08231' // balanceOf function signature
+  const tokenOwner = '0xD00B0Fa59b7ADb92f30e6A402Af28CC42e238343'
+
+  const [xaloBalance, setXaloBalance] = useState(0)
+
+  useEffect(() => {
+    const fetchXaloBalance = async () => {
+      const response = await multicallv2(xaloAbi, [
+        {
+          address: tokenAddress,
+          name: 'balanceOf',
+          params: [tokenOwner],
+        },
+      ])
+      const balance = Number(response[0]) / 10 ** 18
+      setXaloBalance(balance)
+    }
+    if (account) {
+      fetchXaloBalance()
+    } else {
+      setXaloBalance(0)
+    }
+  }, [account, tokenAddress, tokenOwner])
 
   return (
     <>
-      
-      
       <Wrapper>
         <Heading mb="24px" scale="xl" color="white">
           {t('Start in seconds.')}
@@ -103,24 +60,14 @@ const Footer = () => {
         <Link external href="*">
           {t('Learn how to start')}
         </Link>
-        {!account && <ConnectWalletButton mt="24px" />}
-      </Wrapper>
-
-      <Wrapper>
-        <Heading ml="36px" scale="xl" color="black"> 
-          {t('Start in seconds.')}
-        </Heading>
-        <Text textAlign="center" color="green">
-          {t('Connect your crypto wallet to start using the app in seconds.')}
-        </Text>
-        <Text mb="24px" bold color="yellow">
-          {t('No registration needed.')}
-        </Text>
-
-        <Link external href="*">
-          {t('Learn how to start')}
-        </Link>
-        {!account && <ConnectWalletButton mt="24px" />}
+        {account && (
+          <Flex mt="24px" alignItems="center">
+            <Text mr="8px" color="white">
+              {t('Your XALO balance:')}
+            </Text>
+            <BalanceWithLoading value={xaloBalance} color="black" decimals={18} />
+          </Flex>
+        )}
       </Wrapper>
     </>
   )
